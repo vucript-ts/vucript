eb9c46b6e2e7bf12647086698aea1d7e5978bcfe
s19514tt <s19514tt@sfc.keio.ac.jp>
2021-06-12 02:50:16 +0900

    fixed bug caused by typescript version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,3 +3,7 @@ declare namespace Vucript {
     function compileFile(filePath: string, outDir: string): void;
 }
 export default Vucript;
+export type reactive<T> = T;
+export type prop<T> = T;
+export type computed<T> = { (): T };
+export function importComponent(filepath: string, name): void;
